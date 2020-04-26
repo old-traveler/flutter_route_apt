6a745f71d5098d2b4b12a5ace19032c7cbef6641
贺宇成 <heyucheng001@ke.com>
2020-04-26 18:22:16 +0800

    [Add]: [route_generator]

---
+++ example/lib/main.route.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// RouteGenerator
+// **************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:example/page/first_page.dart';
+import 'package:example/page/home_page.dart';
+import 'package:example/page/second_page.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  String routeName = settings.name;
+  switch (routeName) {
+    case 'FirstPage':
+      return MaterialPageRoute(
+          builder: (context) => FirstPage(
+                firstPageTitle: settings.arguments,
+              ));
+    case 'lianjiabeikeft://home':
+      Map<String, dynamic> map = settings.arguments;
+      return MaterialPageRoute(
+          builder: (context) => HomePage(
+                homeTitle: map['homeTitle'],
+                homeTime: map['time'],
+              ));
+    case 'SecondPage':
+      return MaterialPageRoute(
+          builder: (context) => SecondPage(
+                second: settings.arguments,
+              ));
+  }
+  return null;
+}
