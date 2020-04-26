d86ecfacb115cc8f4a94638e66d065986e1b2b70
贺宇成 <heyucheng001@ke.com>
2020-04-26 18:24:29 +0800

    [Add]: [route_generator]

--- example/lib/main.route.dart
+++ example/lib/main.route.dart
@@ -5,17 +5,17 @@
 // **************************************************************************
 
 import 'package:flutter/material.dart';
-import 'package:example/page/first_page.dart';
-import 'package:example/page/home_page.dart';
 import 'package:example/page/second_page.dart';
+import 'package:example/page/home_page.dart';
+import 'package:example/page/first_page.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   String routeName = settings.name;
   switch (routeName) {
-    case 'FirstPage':
+    case 'SecondPage':
       return MaterialPageRoute(
-          builder: (context) => FirstPage(
-                firstPageTitle: settings.arguments,
+          builder: (context) => SecondPage(
+                second: settings.arguments,
               ));
     case 'lianjiabeikeft://home':
       Map<String, dynamic> map = settings.arguments;
@@ -24,10 +24,10 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
                 homeTitle: map['homeTitle'],
                 homeTime: map['time'],
               ));
-    case 'SecondPage':
+    case 'FirstPage':
       return MaterialPageRoute(
-          builder: (context) => SecondPage(
-                second: settings.arguments,
+          builder: (context) => FirstPage(
+                firstPageTitle: settings.arguments,
               ));
   }
   return null;
